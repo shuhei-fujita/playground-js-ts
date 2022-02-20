f0578243877fb051b7c577fbd4c2d244c23ebdd5
syuheifujita <syuheifujita0117@gmail.com>
2022-02-21 03:49:03 +0900

    callback function

--- function/recursive_function.js
+++ function/recursive_function.js
@@ -3,14 +3,13 @@
 
 // 階乗
 function sum(number) {
-    let result = 0;
     console.log(number);
 
     if (number <= 1) {
-        return result += 1; // 通常
+        return 1; // 通常
     }
     
-    return result += number * sum(number - 1);  // 再帰
+    return number * sum(number - 1);  // 再帰
 }
 
 console.log('合計：' + sum(3));
