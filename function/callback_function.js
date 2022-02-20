95d087908b3f3ff766d13af8b975172fbd68ea9b
syuheifujita <syuheifujita0117@gmail.com>
2022-02-21 03:06:35 +0900

    callback function

--- function/callback_function.js
+++ function/callback_function.js
@@ -1,6 +1,13 @@
 // コールバック関数
-function sum() {
+setTimeout(
+    function() {
+        console.log('setTimeout');
+    },
+    1000
+);
 
-}
-
-console.log();
+const arrayNumber = [1, 2, 3, 4, 5];
+const result = arrayNumber.map(function(value) {
+    return value
+});
+console.log(result);
