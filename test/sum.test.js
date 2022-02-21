054a73ab1dab72d5ee250652edf3d1b51d58582b
syuheifujita <syuheifujita0117@gmail.com>
2022-02-21 23:29:32 +0900

    modify

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,9 +1,11 @@
 const sum = require('../function/expression_function.js');
 
+// 一つのtest
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
+// 複数の値を一度に実行するtest
 test.each([
     [1, 2, 3],
     [1, 20, 21],
