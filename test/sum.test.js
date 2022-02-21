28ae5770ac46425d0d9142b7db0b4ec40baa5810
syuheifujita <syuheifujita0117@gmail.com>
2022-02-21 23:28:36 +0900

    modify

--- test/sum.test.js
+++ test/sum.test.js
@@ -3,3 +3,11 @@ const sum = require('../function/expression_function.js');
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+test.each([
+    [1, 2, 3],
+    [1, 20, 21],
+    [1, 200, 201],
+])('adds %i + %i to equal %i', (a, b, result) => {
+    expect(sum(a,b)).toBe(result);
+});
